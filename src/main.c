fd75a48bbdd928f8769bba578118ce12b67cb566
Yurij Boklan <hailrake@riseup.net>
2024-08-11 14:37:59 +0300

    update

--- src/main.c
+++ src/main.c
@@ -14,6 +14,9 @@ typedef enum{NO_RESULT, WIN, LOSE} gameResult;
 
 cellState map[3][3] = { NONE }; 
 gameResult game_result = {NO_RESULT};
+__u_int XWin = 0;
+__u_int OWin = 0;
+__u_int draw = 0;
 
 void drawField(){
     for(int i = 1; i < 3; i++){
@@ -44,8 +47,52 @@ void drawItems(){
     }
 }
 
+void drawStats(){
+    char stats[30];
+    snprintf(stats, 30, "X wins: %d\tO wins: %d\tDraw: %d", XWin, OWin, draw);
+    DrawText(stats, SCREEN_WIDTH / 2 - 200, MARGIN - 50, 36, BLACK);
+}
+
 gameResult checkGameResult(){
+    cellState winSide = NONE; 
+    for(int i = 0; i < 3; i++){
+        if(map[i][0] == map[i][1] && map[i][1] == map[i][2] && map[i][0] != NONE){
+            winSide = map[i][0];
+            break;
+        }
+        if(map[0][i] == map[1][i] && map[1][i] == map[2][i] && map[0][i] != NONE){
+            winSide = map[0][i];
+            break;
+        }
+    }
+    if(map[0][0] == map[1][1] && map[1][1] == map[2][2] && map[0][0] != NONE)
+        winSide = map[0][0];
+    if(map[2][0] == map[1][1] && map[1][1] == map[0][2] && map[2][0] != NONE)
+        winSide = map[2][0];    
+
+
+    if(winSide == X) return WIN;
+    if(winSide == O) return LOSE;
     return NO_RESULT;
+}
+
+bool checkIsMapHaveFreeCell(){
+    int posibleFreeCell = 3*3;        
+    int i = 0;
+    for(int y = 0; y < 3; y++)
+        for(int x = 0; x < 3; x++)
+            if(map[y][x] != NONE)
+                i++;
+
+    return i == posibleFreeCell ? false : true;
+}
+
+void clearMap(){
+      for(int y = 0; y < 3; y++){
+        for(int x = 0; x < 3; x++){
+            map[y][x] = NONE;
+        }
+      }
 }
 
 int main(void) {
@@ -62,9 +109,20 @@ int main(void) {
                 for(int x = 0; x < 3; x++){
                     int cellX = MARGIN + x * CELL_WIDTH;
                     int cellY = MARGIN + y * CELL_HEIGHT;
-                    TraceLog(LOG_INFO, "x: %d y: %d",cellX, cellY);
                     if(mousePosition.x >= cellX && mousePosition.x <= cellX + CELL_WIDTH && mousePosition.y >= cellY && mousePosition.y <= cellY + CELL_HEIGHT && map[y][x] == NONE)
                         map[y][x] = X;
+                }
+            }
+        }
+         if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
+            mousePosition = GetMousePosition();
+
+            for(int y = 0; y < 3; y++){
+                for(int x = 0; x < 3; x++){
+                    int cellX = MARGIN + x * CELL_WIDTH;
+                    int cellY = MARGIN + y * CELL_HEIGHT;
+                    if(mousePosition.x >= cellX && mousePosition.x <= cellX + CELL_WIDTH && mousePosition.y >= cellY && mousePosition.y <= cellY + CELL_HEIGHT && map[y][x] == NONE)
+                        map[y][x] = O;
                 }
             }
         }
@@ -74,15 +132,24 @@ int main(void) {
         BeginDrawing();
             ClearBackground(WHITE);
             
-            //draw field section
+            drawStats();
             drawField();
             drawItems();
             
             if(game_result == WIN){
+                XWin++;
                 TraceLog(LOG_INFO, "WIN!!!");
+                clearMap();
             }
             if(game_result == LOSE){
+                OWin++;
                 TraceLog(LOG_INFO, "LOSE(");
+                clearMap();
+            }
+
+            if(!checkIsMapHaveFreeCell()){
+                draw++;
+                clearMap();
             }
 
         EndDrawing();
